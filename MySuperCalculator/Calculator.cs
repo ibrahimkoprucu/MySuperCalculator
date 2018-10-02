844fb5c835cb5b978e8ec7bd7505b2e94dda7cf5
ibrahim Köprücü <i.koprucu07@gmail.com>
2018-10-03 00:22:59 +0300

    Add project files.

---
+++ MySuperCalculator/Calculator.cs
@@ -0,0 +1,62 @@
+﻿namespace MySuperCalculator
+{
+    public class Calculator
+    {
+        // Method finding greatest common divisor.
+        public int FindGreatestCommonDivisor(int number1, int number2)
+        {
+            int ebob = 1;
+
+            // eğer number1, number2'dan büyük ise
+            if (number1 > number2)
+            {
+                // evet ise
+                // hiçbirşey yapma
+            }
+            else
+            {
+                // büyük sayı küçük sayı yer değiştir
+                int temp;
+                temp = number1;
+
+                number1 = number2;
+                number2 = temp;
+            }
+
+            // kaç kere döneceğini söyleyebilir misin ?
+            // evet ise for , hayır ise while
+
+
+            for (int i = number2; i > 1; i--)
+            {
+                int kalan1 = number1 % i;
+                int kalan2 = number2 % i;
+
+                if (kalan1 == 0 & kalan2 == 0)
+                {
+                    ebob = i;
+
+                    // işim bitti döngüden çıkıyorum.
+                    break;
+                }
+            }
+
+            return ebob;
+        }
+
+        // Euclidean algorithm
+        // https://www.wikizero.pro/index.php?q=aHR0cHM6Ly9lbi53aWtpcGVkaWEub3JnL3dpa2kvRXVjbGlkZWFuX2FsZ29yaXRobQ
+        public ulong GCD(ulong a, ulong b)
+        {
+            while (a != 0 && b != 0)
+            {
+                if (a > b)
+                    a %= b;
+                else
+                    b %= a;
+            }
+
+            return a == 0 ? b : a;
+        }
+    }
+}
---
+++ MySuperCalculator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MySuperCalculator
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            ulong s1 = 1071;
+            ulong s2 = 462;
+
+            // #45232332
+            Calculator ibosCalculator = new Calculator();
+
+            // result = f(10, 15)
+            ulong result = ibosCalculator.GCD(s1, s2);
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
