b1eadc02e95641c779c27d38a00c4350c507e74f
ibrahim Köprücü <i.koprucu07@gmail.com>
2018-10-04 23:37:10 +0300

    milestone

---
+++ Traning/FancyCalculator.cs
@@ -0,0 +1,16 @@
+﻿namespace Traning
+{
+    public class FancyCalculator
+    {
+        /// <summary>
+        /// Bu method bir den başlayarak verilen tamsayıya kadar(x) olan tamsayıların toplamını döner.
+        /// </summary>
+        /// <param name="x">verilen tamsayı</param>
+        /// <returns>toplamı tamsayı(int) tipinde döner</returns>
+        public int SumToGivenNumber(int x)
+        {
+            int sonuc = (x * (x + 1)) / 2;
+            return sonuc;
+        }
+    }
+}
---
+++ Traning/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Traning
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int number1, number2, sum = 0;
+
+            number1 = ReadFromConsoleAsInteger("birinci sayıyı giriniz");
+            number2 = ReadFromConsoleAsInteger("ikinci sayıyı giriniz");
+
+            FancyCalculator myFancyCalculator = new FancyCalculator();
+            int result1 = myFancyCalculator.SumToGivenNumber(number1);
+            int result2 = myFancyCalculator.SumToGivenNumber(number2);
+
+            sum = result2 - result1;
+
+            int mutlakDeger = Math.Abs(sum);
+            Console.WriteLine(mutlakDeger);
+
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Bu method console bir mesaj yazar ve daha sonra kullanıcının girdiği yazıyı tamsayıya çevirip döner.
+        /// </summary>
+        /// <returns>girilen yazının sayıya çevrilmişini (int)</returns>
+        public static int ReadFromConsoleAsInteger(string message)
+        {
+            Console.WriteLine(message);
+            string userInput = Console.ReadLine();
+
+            int userInputAsInt = Convert.ToInt32(userInput);
+            return userInputAsInt;
+        }
+    }
+}
